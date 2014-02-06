19a49b63ad8371f701974fb9b16171761672456d
Karen Kirr <kkirr77@yahoo.com>
2014-02-05 17:27:24 -0800

    update zurb test

--- test/features/check_for_zurb_test.rb
+++ test/features/check_for_zurb_test.rb
@@ -4,7 +4,7 @@ feature "Check to see if Zurb exists yet" do
   scenario "Zurb is in effect on my site" do
       visit root_path
 
-     #  page.must_have_selector (".column")
+      page.must_have_selector (".column")
      #page.source.must_include("foundation.js")
      #page.source_must_include("foundation.css")
     end
