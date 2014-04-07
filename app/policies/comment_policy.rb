1d2ae10fb7a77e9c62ae3835b5c159282d79459e
Karen Kirr <kkirr77@yahoo.com>
2014-04-06 21:12:42 -0700

    update files for login via Twitter

---
+++ app/policies/comment_policy.rb
@@ -0,0 +1,26 @@
+class CommentPolicy
+
+  attr_accessor :user, :comment
+
+  def initialize(user, comment)
+    @user = user
+    @comment = comment
+  end
+
+  def update?
+      @post.author == @user || @user.editor?
+  end
+
+  def create?
+      @user.author? || @user.editor?
+  end
+
+  def publish?
+      @user.editor? || @user.author?
+  end
+
+  def destroy?
+    @user.editor || @user.author
+  end
+
+end
