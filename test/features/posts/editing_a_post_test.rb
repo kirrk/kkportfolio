771298c16e7f8b812a3ae92148fd0b4c8bc288f0
Karen Kirr <kkirr77@yahoo.com>
2014-04-08 15:21:30 -0700

    update with twitter auth

--- test/features/posts/editing_a_post_test.rb
+++ test/features/posts/editing_a_post_test.rb
@@ -5,7 +5,7 @@ feature "Editing a Post" do
 
     #Given an exiting post
    # post = Post.create(title: "Becoming a Code Fellow", body: "Means striving for excellence.")
-    visit post_path(post(:cr))
+    visit post_path(posts(:cr))
 
     #When I click edit and submit changed data
     click_on "Edit"
