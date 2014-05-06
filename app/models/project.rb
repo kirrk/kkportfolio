83616f596d01d2e9bcfda1d138030209c5be9d7b
Karen Kirr <kkirr77@yahoo.com>
2014-05-05 19:36:28 -0700

    done https://app.asana.com/0/11169835259596/11423261480760

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
   attr_accessible :name, :technologies_used
   belongs_to :author, class_name: "User"
-  validates :technologies_used, presence: true
+  validates :technologies_used, presence:true
   validates :name, length: {in: 4..255}
   has_many :comments, as: :commentable
 end
