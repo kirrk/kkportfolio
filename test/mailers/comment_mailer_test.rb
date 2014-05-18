5dddeb24fa624f3619bd3664dbb4ce8659577132
Karen Kirr <kkirr77@yahoo.com>
2014-05-17 20:30:01 -0700

    add email form and letter_opener

--- test/mailers/comment_mailer_test.rb
+++ test/mailers/comment_mailer_test.rb
@@ -2,10 +2,10 @@ require "test_helper"
 
 describe CommentMailer do
   it "comment_confirmation" do
-    mail = CommentMailer.comment_confirmation
-    mail.subject.must_equal "Comment confirmation"
-    mail.to.must_equal ["to@example.org"]
-    mail.from.must_equal ["from@example.com"]
+    mail = CommentMailer.comment_new
+    mail.subject.must_equal "New comment on portfolio'"
+    mail.to.must_equal ["kkirr77@yahoo.com"]
+    mail.from.must_equal ["kkirr77@yahoo.com"]
     mail.body.encoded.must_match "Hi"
   end
 end
