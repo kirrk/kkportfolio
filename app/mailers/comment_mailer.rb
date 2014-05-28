3f2ac758842b210b17f92abaee8974e3b7e50c5a
Karen Kirr <kkirr77@yahoo.com>
2014-05-27 21:03:42 -0700

    update comment_mailer

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -2,7 +2,7 @@ class CommentMailer < ActionMailer::Base
   default from: "kkirr77@yahoo.com"
 
   def comment_new (comment, commentable)
-  @comment = comment,
+  @comment = comment
   @commentable = commentable
     mail to:  'kkirr77@yahoo.com',
          subject: 'New comment on portfolio'
